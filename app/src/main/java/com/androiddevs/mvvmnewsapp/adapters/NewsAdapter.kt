fbf3823b461366c56137a4f9cfb2eabb36147890
VadimKon87 <103271329+VadimKon87@users.noreply.github.com>
2023-01-28 17:28:09 +0200

    MyCommit7 (Step 5 finished - RecyclerView adapter)

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,96 @@
+package com.androiddevs.mvvmnewsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.models.Article
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_article_preview.view.*
+
+// using DiffUtil instead of NotifyDataSetChanged
+
+/* This code defines a class called NewsAdapter, which is a custom implementation of the
+* RecyclerView.Adapter class. This adapter is used to display a list of articles in a RecyclerView.
+
+* The class defines an inner class ArticleViewHolder which takes a single parameter, itemView of type
+* View, and it's the constructor. It is a RecyclerView.ViewHolder class, which is used to hold the
+* views for each item in the list.
+
+* The private val differCallback is an object of DiffUtil.ItemCallback which is used to determine
+* whether two items are the same and whether their contents are the same.
+
+* The differ is an instance of AsyncListDiffer, which is used to calculate the difference between
+* two lists of articles. It uses the differCallback for determining if the items are the same.
+
+* The onCreateViewHolder method is used to inflate the view for each item in the list, and it
+* returns an instance of ArticleViewHolder.
+
+* The onBindViewHolder method is used to bind the data to the views for each item in the list. It
+* takes an ArticleViewHolder and a position as a parameter, it loads the data of the article in the respective views.
+
+* The getItemCount method returns the number of items in the list.
+
+* setOnItemClickListener method takes a lambda function as a parameter and it is used to set a
+* click listener on each item in the list.
+* onItemClickListener is a nullable variable which is used to hold the lambda function that was
+* passed to the setOnItemClickListener method.
+* */
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        // usually we use id instead of url, but since we get items from our API,
+        // they don't have id's, but they have unique url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    // the tool that compares our two lists, will run in background
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_preview,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
+            tvSource.text = article.source.name
+            tvTitle.text = article.title
+            tvDescription.text = article.description
+            tvPublishedAt.text = article.publishedAt
+            setOnItemClickListener {
+                onItemClickListener?.let { it(article) }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -8,10 +8,10 @@ import com.androiddevs.mvvmnewsapp.R
 import kotlinx.android.synthetic.main.activity_news.*
 
 /*
-* Step 0 - renaming MainActivity to NewsActivity (renaming layout to activity_news),
+* STEP 0 - renaming MainActivity to NewsActivity (renaming layout to activity_news),
 *          creating packages for MVVM structure
 *
-* Step 1 - SETTING UP NAVIGATION (navigating between UI fragments)
+* STEP 1 - SETTING UP NAVIGATION (navigating between UI fragments)
 *    1.1 - add newsNavHostFragment to layout, that uses navGraph (not yet created)
 *    1.2 - create all fragment classes (ArticleFragment, BreakingNewsFragment, etc.)
 *    1.3 - create Navigation Graph (res -> new resource file -> Navigation -> news_nav_graph)
@@ -19,7 +19,7 @@ import kotlinx.android.synthetic.main.activity_news.*
 *    1.4 - create bottom_navigation_menu.xml
 *    1.5 - in NewsActivity.kt connect bottomNavigationView with navigation components
 *
-* Step 2 - SETTING UP RETROFIT (for making network requests)
+* STEP 2 - SETTING UP RETROFIT (for making network requests)
 *    2.1 - creating response objects that we need - data classes from JSON that our API returns
 *          (copy a JSON from our API - newsapi.org -> new Kotlin Data class from JSON and as a result
 *          we get 3 data classes - NewsResponse (this name I entered) and Article and Source (created
@@ -27,18 +27,23 @@ import kotlinx.android.synthetic.main.activity_news.*
 *    2.2 - create API interface - NewsAPI.kt
 *    2.3 - create RetrofitInstance - singleton class that allows us to make requests from anywhere in our code
 *
-* Step 3 - SETTING UP ROOM AND DATA ACCESS OBJECT (DAO) - for saving articles to local database
+* STEP 3 - SETTING UP ROOM AND DATA ACCESS OBJECT (DAO) - for saving articles to local database
 *    3.1 - Annotate Article class with @Entity (that way it will be a table in a room data base)
 *    3.2 - Add id parameter and @PrimaryKey(autoGenerate = true) parameter to Article data class
 *    3.3 - Creating DAO interface - ArticleDao.kt, which is like API, but for our local database,
 *          this interface defines functions to insert/updateR, retrieve, delete articles from our local DB.
 *
-* Step 4 - SETTING UP ROOM DATABASE CLASS AND ROOM TYPE CONVERTER
+* STEP 4 - SETTING UP ROOM DATABASE CLASS AND ROOM TYPE CONVERTER
 *    4.1 - create ArticleDatabase.kt abstract class
 *    4.2 - create Converters.kt class - Room only handles primitive data types (int, string, etc),
 *          but our Article class has a source parameter of a custom class Source, which we need to
 *          convert to string using TypeConverter (and also visa versa from String to Source)
 *    4.3 - add converters from 4.2 to ArticleDatabase.kt via annotation
+*
+* STEP 5 - SETTING UP RECYCLERVIEW ADAPTER (WITH DIFFUTIL)
+*    5.1 - create NewsAdapter.kt class in adapters package
+*    5.2 - add AsyncListDiffer to NewsAdapter.kt
+*    5.3 - implement RecyclerView functions (ctrl + i)
 * */
 
 class NewsActivity : AppCompatActivity() {
